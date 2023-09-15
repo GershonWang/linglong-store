4beeb717a4dbc73e9a3c8a96f9faf90c9c85599f
GershonWang <986432015@qq.com>
2023-09-15 11:10:17 +0800

    更新 index.vue

--- src/pages/aboutMenu/index.vue
+++ src/pages/aboutMenu/index.vue
@@ -2,6 +2,17 @@
     <div>
         <h1>关于页面</h1>
         当前系统版本: 0.0.1<br>
-        开发作者：Jokul
+        开发作者：Jokul<br>
+        github地址：<a href="https://github.com/GershonWang/linglong_store">https://github.com/GershonWang/linglong_store</a>
     </div>
 </template>
+<style scoped>
+a {
+  font-weight: bold;
+  color: #D3D3D3;
+  text-decoration: inherit;
+}
+a:hover {
+  color: #2012e9;
+}
+</style>
