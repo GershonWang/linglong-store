849b440b699cbea1f5b81439e37ee34a6b3d93f9
GershonWang <986432015@qq.com>
2023-12-30 08:03:21 +0800

    修改路由配置hashHistory，解决打包后页面加载不出来

--- src/pages/aboutMenu/index.vue
+++ src/pages/aboutMenu/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>关于页面</h1>
-        当前系统版本: 0.0.1<br>
+        当前系统版本: 1.0.1<br>
         开发作者：Jokul<br>
         github地址：<a href="https://github.com/GershonWang/linglong_store">https://github.com/GershonWang/linglong_store</a>
     </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
