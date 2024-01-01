76c51a7fa792a2d817315019fc640ecc69f62ee4
GershonWang <986432015@qq.com>
2024-01-02 01:15:50 +0800

    加说明，更新版本号

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,6 +5,13 @@
         </a>
         <h1>玲珑应用商店</h1>
         <h4>正在进入商店：{{ mins }}秒</h4>
+        <div style="text-align: left;">
+            <a style="color: chocolate;">PS注意：</a>
+          <p>1.刚程序运行时，会检测当前系统是否满足玲珑环境，不满足，倒计时加载会卡住不动并弹出提示，程序不会进入到后续界面;这里需要您手动安装玲珑环境方可使用。</p> 
+          <p>2.点击安装时，受网速和程序大小的影响，程序安装比较缓慢甚至可能会没反应，此时无需操作耐心等待程序安装成功提示即可。</p>
+          <p>3.卸载程序时，第一次点击会提示异常，可以再次点击卸载即可卸载成功。</p>
+          <p>4.玲珑程序搜索功能，暂时未做。</p>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
@@ -14,7 +21,7 @@ import { ElNotification } from 'element-plus'
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-const mins = ref(5);
+const mins = ref(10);
 let timerId: NodeJS.Timer | null = null; // 使用NodeJS.Timer类型
 
 const startTimer = () => {
