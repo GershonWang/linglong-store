6f07fc4df1a13b758dad95be9f6dc67bcea74166
GershonWang <986432015@qq.com>
2024-01-04 22:51:30 +0800

    处理打包配置，添加程序图标文件，解决程序安装后启动器图标显示异常的问题

--- src/pages/mainView/4_AboutMenu/index.vue
+++ src/pages/mainView/4_AboutMenu/index.vue
@@ -1,21 +1,50 @@
 <template>
-    <div>
-        <h1>关于页面</h1>
-        当前系统版本: 1.0.5<br>
-        开发作者：Jokul<br>
-        玲珑官网：<a href="https://linglong.dev/" target="_blank">https://linglong.dev/</a><br>
-        玲珑网页版商店：<a href="https://store.linglong.dev/" target="_blank">https://store.linglong.dev/</a><br>
-        github地址：<a href="https://github.com/GershonWang/linglong_store" target="_blank">https://github.com/GershonWang/linglong_store</a><br>
-        个人介绍：本人是一个菜的不能再菜的菜鸟，当前是Java开发工程师;electron+vite构建该玲珑app商店纯属个人激情，前端vue3是从0.1学起，所以整个项目BUG多多，请指正～
+  <div>
+    <h1>关于页面</h1>
+    <li>
+      <a class="title">项目环境</a>
+    </li>
+    <div class="item">Node版本：{{ versions.node }}</div>
+    <div class="item">Electron版本：{{ versions.electron }}</div>
+    <div class="item">Chrome版本：{{ versions.chrome }}</div>
+    <br>
+    <li>
+      <a class="title">玲珑信息</a>
+    </li>
+    <div class="item">玲珑官网：<a class="link" href="https://linglong.dev/" target="_blank">https://linglong.dev/</a></div>
+    <div class="item">玲珑网页版商店：<a class="link" href="https://store.linglong.dev/" target="_blank">https://store.linglong.dev/</a></div>
+    <br>
+    <li>
+      <a class="title">版本信息</a>
+    </li>
+    <div class="item">当前版本: {{ pkg.version }}</div>
+    <div class="item">开发作者：{{ pkg.author }}</div>
+    <div class="item">
+      github地址：<a class="link" href="https://github.com/GershonWang/linglong_store" target="_blank">https://github.com/GershonWang/linglong_store</a>
     </div>
+  </div>
 </template>
+<script setup lang="ts">
+import pkg from '../../../../package.json';
+const versions = process.versions;
+
+</script>
 <style scoped>
-a {
+.title {
+  font-weight: bold;
+  font-size: 18px;
+  color: #D3D3D3;
+  text-decoration: inherit;
+}
+.item {
+  text-indent: 2em;
+}
+.link {
   font-weight: bold;
   color: #D3D3D3;
   text-decoration: inherit;
 }
-a:hover {
-  color: #2012e9;
+.link:hover {
+  color: #af2f9e;
 }
 </style>
