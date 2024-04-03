2a819f3d85db55b2587c654598dccc50ec59acf0
GershonWang <986432015@qq.com>
2024-04-03 17:07:01 +0800

    更新页面调整

--- src/pages/mainView/4_RuntimeMenu/index.vue
+++ src/pages/mainView/4_RuntimeMenu/index.vue
@@ -1,27 +1,19 @@
 <template>
     <div class="container" v-loading="loading" element-loading-text="加载中...">
-        <div v-if="runtimeList && runtimeList.length > 0">
-            <el-table :data="runtimeList" height="100%" style="width: 100%;border-radius: 5px;">
-                <el-table-column prop="App" label="appId" width="180" />
-                <el-table-column prop="ContainerID" label="容器ID" width="180" />
-                <el-table-column prop="Pid" label="进程ID" width="100" />
-                <el-table-column prop="Path" label="玲珑目录" />
-                <el-table-column fixed="right" label="操作" width="120">
-                    <template #default="scope">
-                        <!-- 停止按钮 -->
-                        <el-button class="uninstallBtn" v-if="!scope.row.isInstalled && !scope.row.loading"
-                            @click="stopPross(scope.row)">停止</el-button>
-                        <el-button v-if="!scope.row.isInstalled && scope.row.loading" loading>停止中</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-        </div>
-        <div class="noDataContainer" v-else>
-            <div class="imageDiv">
-                <img class="image" :src="defaultImage" alt="Image" />
-            </div>
-            <h1>暂无数据</h1>
-        </div>
+        <el-table :data="runtimeList" height="100%" style="width: 100%;border-radius: 5px;">
+            <el-table-column prop="App" label="appId" width="180" />
+            <el-table-column prop="ContainerID" label="容器ID" width="180" />
+            <el-table-column prop="Pid" label="进程ID" width="100" />
+            <el-table-column prop="Path" label="玲珑目录" />
+            <el-table-column fixed="right" label="操作" width="120">
+                <template #default="scope">
+                    <!-- 停止按钮 -->
+                    <el-button class="uninstall-btn" v-if="!scope.row.isInstalled && !scope.row.loading"
+                        @click="stopPross(scope.row)">停止</el-button>
+                    <el-button v-if="!scope.row.isInstalled && scope.row.loading" loading>停止中</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 <script setup lang="ts">
@@ -29,7 +21,6 @@ import { onMounted, onBeforeUnmount, ref } from 'vue';
 import { ipcRenderer } from "electron";
 import { ElNotification } from 'element-plus'
 import { RunTime } from "@/interface/CardFace";
-import defaultImage from '@/assets/logo.svg';
 
 const loading = ref(true);
 let runtimeList = ref([] as RunTime[]);
@@ -93,22 +84,10 @@ onBeforeUnmount(() => {
     overflow-y: auto;
 }
 
-.uninstallBtn {
+.uninstall-btn {
     background-color: red;
     color: white;
     padding: 6px;
     height: 24px;
-}
-
-.noDataContainer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-}
-
-.imageDiv {
-    width: 180px;
-    height: 300px
 }
 </style>
