819c3524b74c2e5265b8d368c0d2513c8ef7b22e
GershonWang <986432015@qq.com>
2024-04-07 15:32:48 +0800

    增加element-plus国际化

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
 import VueLazyload from 'vue-lazyload'
@@ -16,7 +17,9 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 // elementplus
-app.use(ElementPlus);
+app.use(ElementPlus,{
+  locale: zhCn,
+});
 // 注册挂载pinia
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
