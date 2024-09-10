5cbb9e2058ac5c78fa319f0cac1336a6fc9ae34d
Jokul <986432015@qq.com>
2024-09-10 22:44:13 +0800

    全部程序添加loading图标

--- src/pages/mainView/8_AllAppMenu/index.vue
+++ src/pages/mainView/8_AllAppMenu/index.vue
@@ -4,10 +4,8 @@
             <div class="header-left-title">
                 <!-- 使用下拉框的形式展示分类列表，包含全部程序以及来自接口请求的列表 -->
                 <el-select v-model="params.categoryId" placeholder="请选择" @change="handleSearch">
-                    <el-option label="全部程序" value=""></el-option>
                     <el-option v-for="item in categoryList" :key="item.categoryId" :label="item.categoryName"
                         :value="item.categoryId"></el-option>
-                    <!-- 动态生成选项 -->
                 </el-select>
             </div>
         </div>
@@ -18,14 +16,13 @@
             </div>
         </div>
     </div>
-    <div ref="appsContainer" class="apps-container" @scroll="onScroll">
+    <div ref="appsContainer" style="height: calc(100% - 70px);overflow-y: auto;" @scroll="onScroll">
         <div class="card-items-container" v-if="allAppItemsStore.allAppItemList && allAppItemsStore.allAppItemList.length > 0">
             <div class="card-items" v-for="(item, index) in allAppItemsStore.allAppItemList" :key="index">
                 <AllAppCard :name="item.name" :version="item.version" :description="item.description" :arch="item.arch"
                     :isInstalled="item.isInstalled" :appId="item.appId" :icon="item.icon" :loading="item.loading" :zhName="item.zhName"
                     :size="item.size" :categoryName="item.categoryName"/>
             </div>
-            <div v-if="isLoading" class="loading">加载中...</div>
         </div>
         <div class="no-data-container" v-else>
             <div style="width: 180px;height: 300px;">
@@ -34,27 +31,29 @@
             <h1>暂无数据</h1>
         </div>
     </div>
+    <div class="loading-container" :class="{ 'show': isLoading }">
+        <img :src="loadingGIF" width="100%" height="100%" style="border-radius: 15px;"/>
+    </div>
 </template>
 <script setup lang="ts">
 import defaultImage from '@/assets/logo.svg';
-import { Result } from '@/interface';
+import loadingGIF from "@/assets/loading.gif";
 import { nextTick, onMounted, ref } from 'vue';
 import AllAppCard from "@/components/allAppCard.vue";
 import { getSearchAppList, getDisCategoryList } from '@/api/server';
 import router from '@/router';
 import { onBeforeRouteLeave } from 'vue-router';
-import { useInstalledItemsStore } from "@/store/installedItems";
-import { useAllAppItemsStore } from "@/store/allAppItems";
 import { useSystemConfigStore } from "@/store/systemConfig";
+import { useAllAppItemsStore } from "@/store/allAppItems";
+import { useInstalledItemsStore } from "@/store/installedItems";
 
-const installedItemsStore = useInstalledItemsStore();
-const allAppItemsStore = useAllAppItemsStore();
 const systemConfigStore = useSystemConfigStore();
+const allAppItemsStore = useAllAppItemsStore();
+const installedItemsStore = useInstalledItemsStore();
 
 const appsContainer = ref<HTMLDivElement>()
-const categoryList = ref<any[]>([]);
+const categoryList = ref<any[]>([{ "categoryId": "", "categoryName": "全部程序" }]);
 const isLoading = ref<boolean>(false);
-
 const params = ref({ name: '', categoryId: '', repoName: systemConfigStore.defaultRepoName, pageNo: 1, pageSize: 50 })
 
 // 方法：加载更多内容
@@ -68,7 +67,7 @@ const loadMore = async () => {
     if (res.code == 200) {
         res.data.records.forEach(item => {
             item.isInstalled = installedItemsStore.installedItemList.find(it => it.appId == item.appId) ? true : false;
-            item.icon = item.icon?.includes("application-x-executable.svg") ? defaultImage : item.icon;
+            item.icon = !item.icon || item.icon.includes("application-x-executable.svg") ? defaultImage : item.icon;
             allAppItemsStore.addItem(item);
         })
         params.value.pageNo ++;
@@ -101,12 +100,7 @@ onMounted(async () => {
     // 获取分类列表
     let res = await getDisCategoryList();
     if (res.code == 200) {
-        res.data.forEach(item => {
-            categoryList.value.push({
-                categoryId: item.categoryId,
-                categoryName: item.categoryName
-            })
-        })
+        res.data.forEach(item => categoryList.value.push({ categoryId: item.categoryId, categoryName: item.categoryName }))
     }
     // 查询程序展示软件列表
     const meta = router.currentRoute.value.meta;
@@ -122,14 +116,13 @@ onMounted(async () => {
     // 等待下一次 DOM 更新
     await nextTick();
     // 恢复保存的滚动位置
-    const container = document.getElementsByClassName('apps-container')[0] as HTMLDivElement;
-    if (container) {
-        container.scrollTop = Number(meta.savedPosition) || 0;
+    if (appsContainer.value) {
+        appsContainer.value.scrollTop = Number(meta.savedPosition) || 0;
     }
 })
 // 在router路由离开前执行
 onBeforeRouteLeave((to, _from, next) => {
-    to.meta.savedPosition = appsContainer.value?.scrollTop; // 将滚动位置保存到路由元数据中
+    to.meta.savedPosition = appsContainer.value ? appsContainer.value.scrollTop : 0; // 将滚动位置保存到路由元数据中
     to.meta.savedSearchName = params.value.name; // 将搜索名称保存到路由元数据中
     to.meta.savedCategoryId = params.value.categoryId; // 将分类ID保存到路由元数据中
     to.meta.savedPageNo = params.value.pageNo; // 将页码保存到路由元数据中
@@ -138,11 +131,20 @@ onBeforeRouteLeave((to, _from, next) => {
 })
 </script>
 <style scoped>
-.apps-container {
-    height: calc(100% - 70px);
+.loading-container {
+  position: fixed;
+  bottom: 25px;
+  right: 38%;
+  width: 12%;
+  height: 60px; /* 根据需要调整高度 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: transform 1s ease-out;
+  transform: translateY(200%); /* 默认隐藏 */
 }
-.loading {
-  text-align: center;
-  padding: 20px;
+
+.loading-container.show {
+  transform: translateY(0); /* 显示状态 */
 }
 </style>
